99de5d9bf515a0d6a0819e5b864d63cdd063e2f1
[win]Legioner9 <legioner9@inbox.ru>
2021-08-24 17:49:37 +0700

    <> to fascis

--- ArtusFascisOperati/Unitio.v.3/Usu/gicr_operati.js
+++ ArtusFascisOperati/Unitio.v.3/Usu/gicr_operati.js
@@ -2,6 +2,7 @@
 
 const { timer } = require('artus/fascisOperati/g_fascis');
 const { memoizeAsync } = require('artus/fascisOperati/i_fascis');
+const { cancelResume } = require('artus/fascisOperati/cr_fascis');
 
 const fn = (x, callback) => {
   if (typeof x !== 'number') callback('argument is not a number');
@@ -16,17 +17,19 @@ const add = (...data) => {
 const mfn = memoizeAsync(fn).setLengthCache(2).onEventCache('add', add);
 
 const fucus = timer(mfn).setLimit(3).setTimer(1200);
-const fucus3pipe = timer(fucus);
+const fucus3pipe = cancelResume(fucus);
 debugger
-fucus(0, callback);
-fucus.printFn();
+fucus3pipe.cancel()
+fucus3pipe(0, callback);
+fucus3pipe.resume()
+fucus3pipe.printFn();
 
 setTimeout(() => {
-  fucus(1, callback);
-  fucus(2, callback);
+  fucus3pipe(1, callback);
+  fucus3pipe(2, callback);
 }, 100);
 
 setTimeout(() => {
-  fucus(3, callback);
+  fucus3pipe(3, callback);
 }, 500);
 
